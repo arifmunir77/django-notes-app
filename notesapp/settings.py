0b1c53578eb4d115f8ec2b9c0a0ffa4601cde538
Arif Munir <129254701+arifmunir77@users.noreply.github.com>
2024-12-20 17:47:16 +0500

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -91,11 +91,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': 'test_db',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'3306',
+        'PORT': 'db_cont',
     }
 }
 
